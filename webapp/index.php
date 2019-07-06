42fc26ea5d6d3ece9b9d3cf59f2d9c40f818c167
mds <mds750@gmail.com>
2019-07-06 21:32:17 +0200

    initial commit

---
+++ webapp/index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>My Shop</title>
+    </head>
+    <body>
+        <h1>Welcome</h1>
+        <ul>
+            <?php
+                $json = file_get_contents('http://product-service');
+                $obj = json_decode($json);
+                $products = $obj->products;
+                foreach($products as $product) {
+                    echo "<li>$product</li>";
+                }                
+            ?>
+        </ul>
+    </body>
+</html>
